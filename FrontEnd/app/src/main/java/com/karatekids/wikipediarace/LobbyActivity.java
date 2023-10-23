83d614268a3a8593e38b4323a5a828c1486a646f
markmorcos1999 <43058148+markmorcos1999@users.noreply.github.com>
2023-10-23 13:09:27 -0700

    Add progress bar for lobbyactivity + Add files to .gitignore

--- FrontEnd/app/src/main/java/com/karatekids/wikipediarace/LobbyActivity.java
+++ FrontEnd/app/src/main/java/com/karatekids/wikipediarace/LobbyActivity.java
@@ -11,6 +11,15 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        //----- send request to join game----
+
+        //---- receive request to join game -----
+
+        //---- receive response that no other players are waiting in lobby ----
+
+        //to hide loading progress bar:
+        // findViewById(R.id.loading_pb).setVisibility(View.GONE);
     }
 
 }
