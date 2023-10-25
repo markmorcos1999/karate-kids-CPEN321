3f88ccd488d70eb2ebb2397dbfe3355b8da658b3
markmorcos1999 <43058148+markmorcos1999@users.noreply.github.com>
2023-10-25 14:49:04 -0700

    remove token toast and unnecessary comments

--- FrontEnd/app/src/main/java/com/karatekids/wikipediarace/MainActivity.java
+++ FrontEnd/app/src/main/java/com/karatekids/wikipediarace/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
                         // Log and toast
                         String msg = getString(R.string.msg_token_fmt, token);
                         Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 });
 
