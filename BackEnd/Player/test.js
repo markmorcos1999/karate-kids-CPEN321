c51f389e5b3bc0f2d6b3c040cd401383176aaa1c
Mark Mekhail <markm2hs@gmail.com>
2023-10-25 19:30:57 -0700

    Modified and fixed Page and Player Managers

--- BackEnd/Player/test.js
+++ BackEnd/Player/test.js
@@ -1,7 +1,30 @@
 const leaderboardDB = require('./PlayerManager');
 
 db = new leaderboardDB();
-db.connect().then(() => {
-    db.createNewPlayer(300, 'Julian');
-    db.getTopPlayers().then(players => console.log(players));
+db.connect().then(async () => {
+    await db.createNewPlayer(300, 'Julian');
+
+    if (!(await db.playerExists(300))) {
+        console.log("FAILURE");
+    }
+
+    if (await db.playerExists(34234123)) {
+        console.log("FAILURE");
+    }
+
+    if ((await db.getPlayerInfo(300)).elo != 0) {
+        console.log("FAILURE");
+    }
+
+    await db.updatePlayer(300, 100, 1, 0, 20, 29);
+
+    if ((await db.getPlayerInfo(300)).avgGameDuration != 20) {
+        console.log("FAILURE");
+    }
+
+    if ((await db.getTopPlayers()).length != 1) {
+        console.log("FAILURE");
+    }
+
+    console.log("Test completed");
 });
