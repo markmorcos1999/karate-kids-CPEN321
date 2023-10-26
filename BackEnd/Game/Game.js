b2cb140285a5d9b0b5faec67b9789cc4fb9e7629
Mark Mekhail <markm2hs@gmail.com>
2023-10-25 17:32:36 -0700

    Minor backend restructuring

--- BackEnd/Game/Game.js
+++ BackEnd/Game/Game.js
@@ -1,10 +1,29 @@
-module.exports = class Game{
-	
-	
-	
-	constructor(session) {
-		
-		this.id = session.id;
+module.exports = class Session{
 
+
+
+    constructor(id, newPlayers) {
+		
+		this.id = id;
+		
+		for(var pl in newPlayers){
+			pl.sessionId = id;
+		}
+		this.game = null;
+		this.players = newPlayers;
     }
+	
+	addPlayers(newPlayers){
+		for(var pl in newPlayers){
+			pl.sessionId = id;
+			this.players.push(pl)
+		}
+		
+	}
+	
+	startGame(){
+		game = new Game(this);
+	}
+	
+    
 }
