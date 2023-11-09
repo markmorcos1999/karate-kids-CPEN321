19843f27f1c9241aacc44d0646d0223fb9818398
Mark Mekhail <markm2hs@gmail.com>
2023-11-09 10:25:31 -0800

    Add jest support

--- backend/Notifications/test.js
+++
@@ -1,6 +0,0 @@
-const NotificationManager = require('./NotificationManager');
-
-let nm = new NotificationManager();
-
-nm.sendNotificationToDevice('dhr-frYDT16wu0AlIPDknV:APA91bHKNT3iywnREAfeeUPukbudiWSfwY-V1LKCzNg4NS0iTlJB1tU40nQM7HPzDdIIsi08_JrI3IRsohFw7y8ZRks4aavycJsYGTfBS5i0uYBr8Hpy8NmOkgj5AaVI_GyHU9W5u_A2', 'test', 'Hi again, other Mark')
-    .then((success) => console.log("successful: " + success));
--- backend/Page/test.js
+++
@@ -1,8 +0,0 @@
-const PageManager = require('./PageManager.js');
-
-let manager = new PageManager();
-manager.getRandomPages().then(res => {
-    manager.getShortestPath(res[0].title, res[1].title).then(res3 => {
-        console.log(res3);
-    });
-});
