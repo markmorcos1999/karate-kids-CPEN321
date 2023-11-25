15cb3437f050ae7b950501e311026b913128d1f4
JulianKennedy <julian.m.kennedy@gmail.com>
2023-11-24 20:31:17 -0800

    Implemented scoreboard to be clickable and display each user's information

--- frontend/app/src/main/java/com/karatekids/wikipediarace/LeaderboardActivity.java
+++ frontend/app/src/main/java/com/karatekids/wikipediarace/LeaderboardActivity.java
@@ -1,15 +1,31 @@
 package com.karatekids.wikipediarace;
 
+import static android.widget.Toast.*;
+
+import static androidx.core.content.ContextCompat.startActivity;
+import static com.google.android.material.internal.ContextUtils.getActivity;
+import static java.security.AccessController.getContext;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import de.codecrafters.tableview.SortableTableView;
 import de.codecrafters.tableview.TableView;
+import de.codecrafters.tableview.listeners.TableDataClickListener;
 import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
 import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
 import de.codecrafters.tableview.toolkit.TableDataRowBackgroundProviders;
@@ -18,7 +34,9 @@ public class LeaderboardActivity extends AppCompatActivity {
 
     private final static String TAG = "LeaderboardActivity";
 
-    private static final int[] TABLE_HEADERS = { R.string.table_ranking_st, R.string.table_name_st, R.string.table_score_st };
+    public static String JSON_String = "";
+
+    private static final int[] TABLE_HEADERS = {R.string.table_ranking_st, R.string.table_name_st, R.string.table_score_st};
 
     //ChatGPT usage: No
     @Override
@@ -33,23 +51,24 @@ public class LeaderboardActivity extends AppCompatActivity {
         // add the headers for the table
         TableView<String[]> tableView = (TableView<String[]>) findViewById(R.id.leaderboard_table);
         tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, TABLE_HEADERS));
+        tableView.addDataClickListener(new ProfileClickListener());
 
         // create rows of alternating colors
         int colorEvenRows = getResources().getColor(R.color.white);
         int colorOddRows = getResources().getColor(R.color.LightGrey);
         tableView.setDataRowBackgroundProvider(TableDataRowBackgroundProviders.alternatingRowColors(colorEvenRows, colorOddRows));
 
-        String JSON_String = b.getString("leaderboardData");
+        JSON_String = b.getString("leaderboardData");
 
         int count = 1;
 
-        try{
+        try {
             // turn the JSON String into an array
             JSONArray arr = new JSONArray(JSON_String);
             String[][] leaderboardData = new String[Math.min(arr.length(), 10)][3];
 
             // iterate through the array and add each row of data to leaderboardData
-            for(int i = 0; i <= Math.min(arr.length() - 1, 9); i++) {
+            for (int i = 0; i <= Math.min(arr.length() - 1, 9); i++) {
                 String[] rowData = new String[3];
 
                 JSONObject element = arr.getJSONObject(i);
@@ -58,7 +77,7 @@ public class LeaderboardActivity extends AppCompatActivity {
                 rowData[1] = element.getString("name");
                 rowData[2] = element.getString("elo");
 
-                leaderboardData[count-1] = rowData;
+                leaderboardData[count - 1] = rowData;
                 count++;
             }
 
@@ -67,6 +86,27 @@ public class LeaderboardActivity extends AppCompatActivity {
 
         } catch (JSONException e) {
             Log.d(TAG, "Unable to parse leaderboard data from server");
+        }
+    }
+
+
+    private class ProfileClickListener implements TableDataClickListener<String[]> {
+        @Override
+        public void onDataClicked(int rowIndex, String[] player) {
+            try {
+                // turn the JSON String into an array
+                JSONArray arr = new JSONArray(LeaderboardActivity.JSON_String);
+
+                JSONObject element = arr.getJSONObject(rowIndex);
+
+                Intent leaderboardStatisticsIntent = new Intent(LeaderboardActivity.this, LeaderboardStatisticsActivity.class);
+                leaderboardStatisticsIntent.putExtra("playerStats", element.toString());
+                startActivity(leaderboardStatisticsIntent);
+
+
+            } catch (JSONException e) {
+                Log.d(TAG, "Unable to parse leaderboard data from server");
+            }
         }
     }
 }
---
+++ frontend/app/src/main/java/com/karatekids/wikipediarace/LeaderboardStatisticsActivity.java
@@ -0,0 +1,103 @@
+package com.karatekids.wikipediarace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LeaderboardStatisticsActivity extends AppCompatActivity {
+
+    private final static String TAG = "LeaderboardStatisticsActivity";
+    private static final int SILVER_THRESHOLD = 8;
+    private static final int GOLD_THRESHOLD = 10;
+
+    //ChatGPT usage: No
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics_dialog);
+
+        //TODO: display player statistics of games in lifetime
+        TextView games_won = (TextView) findViewById(R.id.games_won_value_tv);
+        TextView games_lost = (TextView) findViewById(R.id.games_lost_value_tv);
+        TextView elo = (TextView) findViewById(R.id.elo_value_tv);
+        TextView player_id = (TextView) findViewById(R.id.player_id_value_tv);
+        TextView player_name = (TextView) findViewById(R.id.player_name_tv);
+
+        Bundle b = getIntent().getExtras();
+
+        String JSON_String = b.getString("playerStats");
+
+        try{
+            JSONObject obj = new JSONObject(JSON_String);
+            games_won.setText(obj.getString("gamesWon"));
+            games_lost.setText(obj.getString("gamesLost"));
+            elo.setText(obj.getString("elo"));
+            player_id.setText(obj.getString("_id"));
+            player_name.setText(obj.getString("name"));
+
+            int gamesWon = Integer.parseInt(obj.getString("gamesWon"));
+
+            if(gamesWon >= GOLD_THRESHOLD) {
+                ImageView image = (ImageView) findViewById(R.id.badge_iv);
+                image.setImageResource(R.drawable.gold_badge);
+            } else if (gamesWon < GOLD_THRESHOLD && gamesWon >= SILVER_THRESHOLD) {
+                ImageView image = (ImageView) findViewById(R.id.badge_iv);
+                image.setImageResource(R.drawable.silver_badge);
+            }
+
+        } catch (JSONException e) {
+            Log.d(TAG, "Unable to parse statistics data from server");
+        }
+    }
+
+    //ChatGPT usage: No
+    // https://www.geeksforgeeks.org/how-to-add-share-button-in-toolbar-in-android/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // first parameter is the file for icon and second one is menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //ChatGPT usage: No
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // We are using switch case because multiple icons can be kept
+        switch (item.getItemId()) {
+            case R.id.shareButton:
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+                // type of the content to be shared
+                sharingIntent.setType("text/plain");
+
+                // Body of the content
+                String shareBody = "Share your stats with friends!";
+
+                // subject of the content. you can share anything
+                String shareSubject = "Share stats";
+
+                // passing body of the content
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+                // passing subject of the content
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                break;
+
+            default:
+                Log.d(TAG, "Button could not be found.");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
